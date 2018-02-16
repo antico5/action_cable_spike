3c5001ecf34ead0fc57a3cd6b1bd60ec6ea30832
Armando Andini <armando.andini@hotmail.com>
2018-02-16 15:11:25 -0300

    Create core classes

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_many :followee_followings, class_name: 'Following', foreign_key: :follower_id, dependent: :destroy
+  has_many :follower_followings, class_name: 'Following', foreign_key: :followee_id, dependent: :destroy
+
+  has_many :followees, through: :followee_followings, class_name: "User"
+  has_many :followers, through: :follower_followings, class_name: "User"
+
+  def follow user
+    followee_followings.create followee: user
+  end
+end
